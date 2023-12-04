e2a9409f9e43c32550da8395e8ed518f06e771d4
Amiri <iuak228@gmail.com>
2023-12-04 21:22:36 +0300

    Initial commit

---
+++ common/src/main/java/io/github/cdagaming/crystaloptimizer/CrystalOptimizer.java
@@ -0,0 +1,9 @@
+package io.github.cdagaming.crystaloptimizer;
+
+public class CrystalOptimizer {
+    public static final String MOD_ID = "crystaloptimizer";
+
+    public static void init() {
+        // N/A
+    }
+}
